79948e9a271fbcf7dfdcc19050901c2074ef8337
KaranAgrawal752 <kannuagrawal752@gmail.com>
2021-09-19 12:58:09 +0530

    databse server changed

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'miniproj');
+define('DB_SERVER', 'sql3.freemysqlhosting.net');
+define('DB_USERNAME', 'sql3438124');
+define('DB_PASSWORD', 'zf42J2UgvB');
+define('DB_NAME', 'sql3438124');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
